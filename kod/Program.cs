2909f3f099561a5fa22778cab4664245f10c0a49
Matvey <mat-lad@mail.ru>
2022-06-18 12:37:37 +0300

    решение задачи

---
+++ kod/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] countries = {"RSA", "USA", "Russia", "England", "Spain", "Chaina", "CAR", "UAE", "Iran", "Aruba"};
+string[] result = new string[countries.Length];
+
+
+void NewArr(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void Print(string[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.Write(mas[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+NewArr(countries, result);
+Print(countries);
+Console.WriteLine();
+Print(result);
